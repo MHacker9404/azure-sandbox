86a8ecfeeb938e66a55cf42acf699609d69b7890
Phil Boyd <philip.r.boyd@saic.com>
2021-08-30 14:46:23 -0400

    chore(repo): initial add

---
+++ AzureFunctionTimer/AzureFunctionTimer/Function1.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionTimer
+{
+    [StorageAccount("AzureWebJobsStorage")]
+    public static class Function1
+    {
+        [FunctionName("AzureFunctionTimerTriggered")]
+        public static void Run([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer,
+            [Queue("the-queue", Connection = "AzureWebJobsStorage")] out string qMessage,
+            ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            qMessage = "Timer triggered";
+        }
+    }
+}
---
+++ AzureFunctionTimer/AzureFunctionTimer/Function2.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionTimer
+{
+    [StorageAccount("AzureWebJobsStorage")]
+    public static class Function2
+    {
+        [FunctionName("AzureFunctionQueueTriggered")]
+        public static void Run([QueueTrigger("the-queue", Connection = "AzureWebJobsStorage")]string myQueueItem,
+            [Blob("the-blobs/{queueTrigger}", FileAccess.Read, Connection = "AzureWebJobsStorage")] Stream myPic,
+                ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            if (myPic != null)
+            {
+                if (myQueueItem.ToUpper().Contains("SECURITY"))
+                {
+                    log.LogInformation($"Security Image found picSize: {myPic.Length} bytes");
+                }
+                else
+                {
+                    log.LogInformation($"New Image found picSize: {myPic.Length} bytes");
+                }
+            }
+        }
+    }
+}
