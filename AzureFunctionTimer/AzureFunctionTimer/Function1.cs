5feb38b94034b128e9ffe7214837a600d81f1a02
Phil Boyd <phil.boyd@csat.com>
2022-06-13 17:50:39 -0400

    stuff

--- AzureFunctionTimer/AzureFunctionTimer/Function1.cs
+++ AzureFunctionTimer/AzureFunctionTimer/Function1.cs
@@ -4,16 +4,16 @@ using Microsoft.Extensions.Logging;
 
 namespace AzureFunctionTimer
 {
-    [StorageAccount("AzureWebJobsStorage")]
-    public static class Function1
+  [StorageAccount("AzureWebJobsStorage")]
+  public static class Function1
+  {
+    [FunctionName("AzureFunctionTimerTriggered")]
+    public static void Run([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer,
+        [Queue("the-queue", Connection = "AzureWebJobsStorage")] out string qMessage,
+        ILogger log)
     {
-        [FunctionName("AzureFunctionTimerTriggered")]
-        public static void Run([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer,
-            [Queue("the-queue", Connection = "AzureWebJobsStorage")] out string qMessage,
-            ILogger log)
-        {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            qMessage = "Timer triggered";
-        }
+      log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+      qMessage = "Timer triggered";
     }
+  }
 }
--- AzureFunctionTimer/AzureFunctionTimer/Function2.cs
+++ AzureFunctionTimer/AzureFunctionTimer/Function2.cs
@@ -6,26 +6,26 @@ using Microsoft.Extensions.Logging;
 
 namespace AzureFunctionTimer
 {
-    [StorageAccount("AzureWebJobsStorage")]
-    public static class Function2
+  [StorageAccount("AzureWebJobsStorage")]
+  public static class Function2
+  {
+    [FunctionName("AzureFunctionQueueTriggered")]
+    public static void Run([QueueTrigger("the-queue", Connection = "AzureWebJobsStorage")] string myQueueItem,
+        [Blob("the-blobs/{queueTrigger}", FileAccess.Read, Connection = "AzureWebJobsStorage")] Stream myPic,
+            ILogger log)
     {
-        [FunctionName("AzureFunctionQueueTriggered")]
-        public static void Run([QueueTrigger("the-queue", Connection = "AzureWebJobsStorage")]string myQueueItem,
-            [Blob("the-blobs/{queueTrigger}", FileAccess.Read, Connection = "AzureWebJobsStorage")] Stream myPic,
-                ILogger log)
+      log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+      if (myPic != null)
+      {
+        if (myQueueItem.ToUpper().Contains("SECURITY"))
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-            if (myPic != null)
-            {
-                if (myQueueItem.ToUpper().Contains("SECURITY"))
-                {
-                    log.LogInformation($"Security Image found picSize: {myPic.Length} bytes");
-                }
-                else
-                {
-                    log.LogInformation($"New Image found picSize: {myPic.Length} bytes");
-                }
-            }
+          log.LogInformation($"Security Image found picSize: {myPic.Length} bytes");
         }
+        else
+        {
+          log.LogInformation($"New Image found picSize: {myPic.Length} bytes");
+        }
+      }
     }
+  }
 }
